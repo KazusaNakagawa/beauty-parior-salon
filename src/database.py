f5d72ca71d24df57f4dea4d044a4e9c2480338d2
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-02-26 14:40:26 +0900

    fefactor: mysql setup

--- src/database.py
+++ src/database.py
@@ -1,14 +1,27 @@
-from sqlalchemy import create_engine
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData
 
-# Dockerで起動すると、docker側で、sql_app.db が生成される
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+load_dotenv()
+
+_DB_PORT = os.getenv('DB_PORT')
+_USER = os.getenv('MYSQL_USER')
+_MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
+_DATABASE = os.getenv('DATABASE')
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{_USER}:{_PASSWORD}@{_MYSQL_DATABASE}:{_DB_PORT}/{_DATABASE}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+meta = MetaData()
+conn = engine.connect()
 
 Base = declarative_base()
