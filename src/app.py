891d17e723a15d3d00a66c83131788c40ce4d9c6
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-03-24 21:48:26 +0900

    test: commit test

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, File, Form, UploadFile
 from mangum import Mangum
 
@@ -67,6 +68,14 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.delete("/users/{user_id}", response_model=schemas.User)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.delete_user(db, user_id=user_id)
+    if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
@@ -143,3 +152,6 @@ async def create_file(
 
 
 handler = Mangum(app)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
