66bc110b5f5aa943e02b3e6fb769c8d66fc09978
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-04-05 16:15:05 +0900

    feat: Narrow down to only the codes you need

--- src/app.py
+++ src/app.py
@@ -23,6 +23,8 @@ from typing import Optional
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
+import pdb
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -64,108 +66,108 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.delete("/users/{user_id}", response_model=schemas.User)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.delete_user(db, user_id=user_id)
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-        user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
-
-
-@app.post("/files/")
-async def create_file(file: bytes = File(...)):
-    """
-    curl -X 'POST' \
-      'http://0.0.0.0:8000/files/' \
-      -H 'accept: application/json' \
-      -H 'Content-Type: multipart/form-data' \
-      -F 'file=@sample.m4a;type=audio/x-m4a'
-
-    :param file:
-    :return:
-    """
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(upload_file: UploadFile):
-    """
-    curl -X 'POST' \
-      'http://0.0.0.0:8000/uploadfile/' \
-      -H 'accept: application/json' \
-      -H 'Content-Type: multipart/form-data' \
-      -F 'file=@sample.m4a;type=audio/x-m4a'
-
-    :param upload_file:
-    :return:
-    """
-    return {"filename": upload_file.filename}
-
-
-@app.post("/files-from/")
-async def create_file(
-        file: bytes = File(...),
-        fileb: UploadFile = File(...),
-        manager_user_id: str = Form(...),
-):
-    """
-    curl -X 'POST' \
-      'http://localhost:8000/files-from/' \
-      -H 'accept: application/json' \
-      -H 'Content-Type: multipart/form-data' \
-      -F 'file=@sample.m4a;type=audio/x-m4a' \
-      -F 'fileb=@sample.m4a;type=audio/x-m4a' \
-      -F 'manager_user_id=m000001'
-
-    :param file:
-    :param fileb:
-    :param manager_user_id:
-
-    :return:
-    """
-    return {
-        "file_size": len(file),
-        "fileb_content_type": fileb.content_type,
-        "manager_user_id": manager_user_id,
-    }
-
-
-handler = Mangum(app)
+# @app.post("/users/", response_model=schemas.User)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="Email already registered")
+#     return crud.create_user(db=db, user=user)
+#
+#
+# @app.get("/users/", response_model=list[schemas.User])
+# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     users = crud.get_users(db, skip=skip, limit=limit)
+#     return users
+#
+#
+# @app.get("/users/{user_id}", response_model=schemas.User)
+# def read_user(user_id: int, db: Session = Depends(get_db)):
+#     db_user = crud.get_user(db, user_id=user_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+#
+#
+# @app.delete("/users/{user_id}", response_model=schemas.User)
+# def delete_user(user_id: int, db: Session = Depends(get_db)):
+#     db_user = crud.delete_user(db, user_id=user_id)
+#     if not db_user:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+#
+#
+# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
+# def create_item_for_user(
+#         user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+# ):
+#     return crud.create_user_item(db=db, item=item, user_id=user_id)
+#
+#
+# @app.get("/items/", response_model=list[schemas.Item])
+# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     items = crud.get_items(db, skip=skip, limit=limit)
+#     return items
+#
+#
+# @app.post("/files/")
+# async def create_file(file: bytes = File(...)):
+#     """
+#     curl -X 'POST' \
+#       'http://0.0.0.0:8000/files/' \
+#       -H 'accept: application/json' \
+#       -H 'Content-Type: multipart/form-data' \
+#       -F 'file=@sample.m4a;type=audio/x-m4a'
+#
+#     :param file:
+#     :return:
+#     """
+#     return {"file_size": len(file)}
+#
+#
+# @app.post("/uploadfile/")
+# async def create_upload_file(upload_file: UploadFile):
+#     """
+#     curl -X 'POST' \
+#       'http://0.0.0.0:8000/uploadfile/' \
+#       -H 'accept: application/json' \
+#       -H 'Content-Type: multipart/form-data' \
+#       -F 'file=@sample.m4a;type=audio/x-m4a'
+#
+#     :param upload_file:
+#     :return:
+#     """
+#     return {"filename": upload_file.filename}
+#
+#
+# @app.post("/files-from/")
+# async def create_file(
+#         file: bytes = File(...),
+#         fileb: UploadFile = File(...),
+#         manager_user_id: str = Form(...),
+# ):
+#     """
+#     curl -X 'POST' \
+#       'http://localhost:8000/files-from/' \
+#       -H 'accept: application/json' \
+#       -H 'Content-Type: multipart/form-data' \
+#       -F 'file=@sample.m4a;type=audio/x-m4a' \
+#       -F 'fileb=@sample.m4a;type=audio/x-m4a' \
+#       -F 'manager_user_id=m000001'
+#
+#     :param file:
+#     :param fileb:
+#     :param manager_user_id:
+#
+#     :return:
+#     """
+#     return {
+#         "file_size": len(file),
+#         "fileb_content_type": fileb.content_type,
+#         "manager_user_id": manager_user_id,
+#     }
+#
+#
+# handler = Mangum(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
@@ -190,7 +192,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 fake_users_db = {
     "john": {
         "username": "john",
-        "full_name": "John Doe",
+        # "full_name": "John Doe",
         "email": "johndoe@example.com",
         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
@@ -210,7 +212,7 @@ class TokenData(BaseModel):
 class User(BaseModel):
     username: str
     email: Optional[str] = None
-    full_name: Optional[str] = None
+    # full_name: Optional[str] = None
     disabled: Optional[bool] = None
 
 
@@ -227,11 +229,36 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
+# def get_password_hash(password):
+#     return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
+    """
+    >>> db: dict
+    {
+     'john': {
+       'username': 'john',
+       'full_name': 'John Doe',
+       'email': 'johndoe@example.com',
+       'hashed_password': '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW',
+       'disabled': False
+      }
+    }
+
+    >>> db[username]: dict
+    {
+     'username': 'john',
+     'full_name': 'John Doe',
+     'email': 'johndoe@example.com',
+     'hashed_password': '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW',
+     'disabled': False
+    }
+
+    :param db:
+    :param username:
+    :return:
+    """
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
@@ -247,6 +274,13 @@ def authenticate_user(fake_db, username: str, password: str):
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """
+
+    :param data: {'sub': 'john'}
+    :param expires_delta: datetime.timedelta(seconds=1800)
+    :return:
+    """
+    pdb.set_trace()
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -265,6 +299,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        print('payload: L296', payload)
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -278,6 +313,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    print('current_user: L309', current_user)
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
@@ -285,7 +321,11 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    """ API: login
+    TODO: fake_user_db -> db
+    """
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    print('user L317', user)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
